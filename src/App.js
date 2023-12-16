ba2759f23ee9aa4da232e325b89baf3a2e8c23cf
Sandra Krejcir <krejcir.sandra@gmail.com>
2023-12-16 18:20:26 +0100

    adding the fetchUser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MyApplications from "./pages/my-applications";
+import ApplicationDetails from "./pages/application-details";
+import { useEffect, useState } from "react";
 import MyProfile from "./pages/my-profile";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const fetchUser = () => {
+    fetch(`http://localhost:3005/user/3`)
+      .then((response) => response.json())
+      .then((userData) => {
+        setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return <MyProfile />;
 }
 
