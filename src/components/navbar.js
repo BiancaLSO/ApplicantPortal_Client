19760dd211ae22157b1665e5958fc8c39d009157
Bianca Stan <biancastan90@gmail.com>
2023-12-22 11:34:45 +0100

    last desig for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,14 +10,6 @@ const Navbar = () => {
         <img src={logo} alt="logo" />
       </div>
       <div className="navbar-menu">
-        {/* <p>MY APPLICATIONS</p>
-        <p>CREATE APPLICATIONS</p>
-        <p>MY PROFILE</p>
-        <div className="logout-icon-container">
-          <span class="material-symbols-outlined">logout</span>
-        </div> */}
-
-        {/* Uncomment these after the routing is done */}
         <NavLink to="/applications" activeClassName="selected">
           MY APPLICATIONS
         </NavLink>
@@ -27,13 +19,11 @@ const Navbar = () => {
         <NavLink to="/profile" activeClassName="selected">
           MY PROFILE
         </NavLink>
-        {/* <NavLink
-          to="/logout"
-          activeClassName="selected"
-          className="logout-link"
-        >
-          <span className="material-symbols-outlined">logout</span>
-        </NavLink> */}
+        <NavLink to="/login" activeClassName="selected" className="logout-link">
+          <span className="material-symbols-outlined logout-icon-container">
+            logout
+          </span>
+        </NavLink>
       </div>
     </nav>
   );
