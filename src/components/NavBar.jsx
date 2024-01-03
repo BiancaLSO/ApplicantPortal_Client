f9db4692ca7e117af5d9f43df092b5b6827d0890
Bianca Stan <biancastan90@gmail.com>
2024-01-03 14:02:13 +0100

    navbar hide btns

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,40 @@
 import React from "react";
 import "../css/navbar.css";
 import logo from "../assests/icons/logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  const routesWithoutButtons = ["/auth/signup", "/auth/login"];
+  const hideButtons = routesWithoutButtons.includes(location.pathname);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <img src={logo} alt="logo" />
       </div>
-      <div className="navbar-menu">
-        <NavLink to="/applications" activeClassName="selected">
-          MY APPLICATIONS
-        </NavLink>
-        <NavLink to="/grants" activeClassName="selected">
-          CREATE APPLICATIONS
-        </NavLink>
-        <NavLink to="/profile" activeClassName="selected">
-          MY PROFILE
-        </NavLink>
-        <NavLink to="/login" activeClassName="selected" className="logout-link">
-          <span className="material-symbols-outlined logout-icon-container">
-            logout
-          </span>
-        </NavLink>
-      </div>
+      {!hideButtons && (
+        <div className="navbar-menu">
+          <NavLink to="/applications" activeClassName="selected">
+            MY APPLICATIONS
+          </NavLink>
+          <NavLink to="/grants" activeClassName="selected">
+            CREATE APPLICATIONS
+          </NavLink>
+          <NavLink to="/profile" activeClassName="selected">
+            MY PROFILE
+          </NavLink>
+          <NavLink
+            to="/login"
+            activeClassName="selected"
+            className="logout-link"
+          >
+            <span className="material-symbols-outlined logout-icon-container">
+              logout
+            </span>
+          </NavLink>
+        </div>
+      )}
     </nav>
   );
 };
